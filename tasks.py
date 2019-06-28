79e79389f45981b9e4ce1d0a075eb317d3134268
Makavura <makavuramughanga@gmail.com>
2019-06-28 05:20:08 +0300

    create tasks model using tasks json format

--- tasks.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify, abort, make_response, request
-
-app = Flask(__name__)
-
-tasks = []
-
-
-if __name__ == '__main__':
-    app.run(degug=True)
---
+++ tasks_model.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+
+class Tasks(object):
+
+    tasks = []
+
+    def __init__(self, title, description):
+        self.id = len(self.tasks)+1
+        self.title = title
+        self.description = description
+        self.createdDate = datetime.now()
+        self.startDate = datetime.now()
+        self.endDate = None
+
+
+    def create_task(self):
+        task = dict(
+            id = self.id,
+            title = self.title,
+            description = self.description,
+            createdDate = self.createdDate,
+            startDate = self.startDate,
+            endDate = self.endDate
+        )
+        Tasks.tasks.append(task)
+        return task
+
+    def get_tasks(self):
+        return Tasks.tasks
+
+    def get_task_by_id(self, id):
+        for task in Tasks.tasks:
+            if task['id'] == id:
+                return task
+
+    def get_task_by_title(self, title):
+        for task in Tasks.tasks:
+            if task['title'] == title:
+                return task
+
+    def update_task(self, id, title, description):
+        task = Tasks.get_task_by_id(self, id)
+        if task['id'] == id:
+            task['title'] = title
+            task['description'] = description
+
+            return task
