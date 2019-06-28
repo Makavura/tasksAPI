bf01d625e97e287913373c14a423b53c75a29683
Makavura <makavuramughanga@gmail.com>
2019-06-28 05:43:44 +0300

    create tasks view and routes

---
+++ tasks_view.py
@@ -0,0 +1,31 @@
+from flask import make_response, jsonify, Flask, request, Blueprint, make_response
+from tasks_model import Tasks
+
+tasks = Blueprint('tasks', __name__, url_prefix='/taskMS/api/v1')
+
+@tasks.route('/tasks', method=['POST'])
+def create_task():
+    data = request.json()
+
+    title = data['title']
+    description = data['description']
+    createdDate = data['createdDate']
+    startDate = data['startDate']
+    endDate = data['endDate']
+
+    payload = Tasks(title, description)
+    new_task = payload.create_task()
+    response_message = {
+        "status": "success",
+        "message": "task created successfully"
+
+    }
+    return jsonify(new_task, response_message), 201
+
+@tasks.route('/tasks', method=['GET'])
+def get_tasks():
+    return jsonify(tasks), 200
+
+@tasks.route('/tasks/<int:id>', method=['GET'])
+def get_task():
+    return jsonify(tasks[int(id)-1])
