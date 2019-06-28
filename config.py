12152bbf97ccd44fe1686c07bf3ca7c49a784fe0
Makavura <makavuramughanga@gmail.com>
2019-06-28 05:58:28 +0300

    create configuration settings

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+
+
+class Configuration(object):
+    """Configuration Class """
+    DEBUG = False
+    SECRET_KEY = os.getenv('SECRET_KEY', 'tinkerbells')
+    CSRF_ENABLED = True
+
+
+class DevelopmentConfiguration(Configuration):
+    """Develepment"""
+    DEBUG = True
+
+
+class TestingConfiguration(Configuration):
+    """Tests Configurations"""
+    TESTING = True
+    DEBUG = True
+
+
+class ProductionConfiguration(Configuration):
+    """Production Deployment"""
+    DEBUG = False
+    TESTING = False
+
+
+config_by_name = {
+    'development': DevelopmentConfiguration,
+    'testing': TestingConfiguration,
+    'production': ProductionConfiguration,
+}
