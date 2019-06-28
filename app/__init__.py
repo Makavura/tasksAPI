8625b081eeb01d03bd6c8c9e8c18066b9d6809e6
Makavura <makavuramughanga@gmail.com>
2019-06-28 06:52:04 +0300

    update import reference

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from instance.config import *
-from app.views.tasks_view import *
+from app.views.tasks_view import tasks
 
 def create_app(config_by_name):
     app = Flask(__name__)
